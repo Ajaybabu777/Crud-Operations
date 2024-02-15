000051664de0284fec59ffa0921ae2f5e9524944
Ajaybabu777 <ajaybabupenumalli@gmail.com>
2024-02-15 20:57:28 +0530

    Employee intailiation completed

---
+++ emoloyees/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import get_object_or_404, render
+from .models import *
+from django.http import Http404
+
+# Create your views here.
+def employee_detail(request,pk):
+    employee = get_object_or_404(Employee,pk=pk)
+    context = {
+        'employee':employee,
+    }
+    return render(request,'employee_detail.html',context)
+   
+    
+
+        
